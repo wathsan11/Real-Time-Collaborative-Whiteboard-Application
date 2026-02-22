a1f95b38f4ea858c8a1e941bfb49d40285040e9e
wathsan11 <wathsaninduwara@gmail.com>
2026-02-22 13:54:05 +0530

    frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+const App = () => {
+ 
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Real-Time Collaborative Whiteboard</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
